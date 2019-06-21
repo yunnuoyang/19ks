aae8ceea2b3a8973f9fbb3ab10f00de155f2dff8
yunnuoyang <50757618+yunnuoyang@users.noreply.github.com>
2019-06-21 21:04:28 +0800

    shiro+

---
+++ bwl_ssm_service/src/main/java/com/bwl/service/serviceimpl/IUserServiceDetail.java
@@ -0,0 +1,32 @@
+package com.bwl.service.serviceimpl;
+
+import com.bwl.mapper.UserMapper;
+import com.bwl.pojo.SecurityUser;
+import com.bwl.pojo.User;
+import com.bwl.pojo.UserExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("userService")
+public class IUserServiceDetail implements UserDetailsService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andUsernumEqualTo(s);
+        List<User> users = userMapper.selectByExample(userExample);
+        User user=null;
+        if(users.size()>0) {
+            user = users.get(0);
+        }
+        SecurityUser securityUser = new SecurityUser();
+        return securityUser;
+    }
+}
